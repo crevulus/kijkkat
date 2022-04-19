3aa46643b163142acc86039e3684cbf3768ebbee
Christopher Evans <christopher.evans@mediamonks.com>
2022-04-19 17:41:00 +0200

    add nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import {
+  BrowserRouter,
+  Link as RouterLink,
+  Routes,
+  Route,
+} from "react-router-dom";
 import "./App.css";
 
 import PetsIcon from "@mui/icons-material/Pets";
@@ -11,12 +17,27 @@ import {
   BottomNavigationAction,
   Container,
   Snackbar,
+  CircularProgress,
 } from "@mui/material";
 
 import { getFirestore, collection } from "firebase/firestore";
 import { useCollection } from "react-firebase-hooks/firestore";
 
 import { firebaseApp } from "./firebase";
+
+const container = (text: string, loading: boolean) => {
+  return (
+    <Container sx={{ flexGrow: 1 }}>
+      {loading ? (
+        <CircularProgress />
+      ) : (
+        <Typography variant="h1" gutterBottom>
+          {text}
+        </Typography>
+      )}
+    </Container>
+  );
+};
 
 function App() {
   const [value, setValue] = useState(0);
@@ -48,22 +69,43 @@ function App() {
 
   return (
     <div className="App">
-      <Container sx={{ flexGrow: 1 }}>
-        <Typography variant="h1" gutterBottom>
-          Kijkkat
-        </Typography>
-      </Container>
-      <BottomNavigation
-        showLabels
-        value={value}
-        onChange={(_, newValue) => {
-          setValue(newValue);
-        }}
-      >
-        <BottomNavigationAction label="Cats" icon={<PetsIcon />} />
-        <BottomNavigationAction label="Map" icon={<LocationOnIcon />} />
-        <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-      </BottomNavigation>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={container("Home", loading)} />
+          <Route path="/map" element={container("Map", loading)} />
+          <Route
+            path="/favourites"
+            element={container("Favourites", loading)}
+          />
+        </Routes>
+
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(_, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction
+            label="Cats"
+            icon={<PetsIcon />}
+            component={RouterLink}
+            to="/"
+          />
+          <BottomNavigationAction
+            label="Map"
+            icon={<LocationOnIcon />}
+            component={RouterLink}
+            to="/map"
+          />
+          <BottomNavigationAction
+            label="Favourites"
+            icon={<FavoriteIcon />}
+            component={RouterLink}
+            to="/favourites"
+          />
+        </BottomNavigation>
+      </BrowserRouter>
       <Snackbar
         open={errorSnackbarOpen}
         autoHideDuration={6000}
