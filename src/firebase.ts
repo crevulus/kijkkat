dc451c74e465bca9fd702d0de18e9f888cca0396
Christopher Evans <christopher.evans@mediamonks.com>
2022-04-21 15:00:25 +0200

    Add env vars to github workflow

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,9 +2,9 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  authDomain: "kijkkat-meow.firebaseapp.com",
+  projectId: "kijkkat-meow",
+  storageBucket: "kijkkat-meow.appspot.com",
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSENGER_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
