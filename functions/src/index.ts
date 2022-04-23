07ed285b689f4aa29e15f7e7775edbd1d4e8021f
Christopher Evans <christopher.evans@mediamonks.com>
2022-04-23 10:58:33 +0200

    cat and nsfw search on image upload

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,17 +5,50 @@ const vision = require("@google-cloud/vision");
 
 admin.initializeApp();
 
+type LabelType = {
+  description: string;
+} & { [key: string]: unknown };
+
+enum SafeSearchLikelihoods {
+  UNKNOWN = "UNKNOWN",
+  VERY_UNLIKELY = "VERY_UNLIKELY",
+  UNLIKELY = "UNLIKELY",
+  POSSIBLE = "POSSIBLE",
+  LIKELY = "LIKELY",
+  VERY_LIKELY = "VERY_LIKELY",
+}
+
+const catLabelsArray = [
+  "cat",
+  "cats",
+  "kitten",
+  "kittens",
+  "siamese",
+  "felidae",
+  "small to medium-sized cats",
+];
+
 exports.checkForCats = functions
   .region("europe-west1")
-  .storage.bucket("cats")
-  .object()
+  .storage.object()
   .onFinalize(async (object) => {
     const client = new vision.ImageAnnotatorClient();
     const [result] = await client.labelDetection(
       `gs://${object.bucket}/${object.name}`
     );
-    console.log(result);
+    const [safeSearchResult] = await client.safeSearchDetection(
+      `gs://${object.bucket}/${object.name}`
+    );
     const labels = result.labelAnnotations;
-    console.log("Labels:");
-    labels.forEach((label: any) => console.log(label.description));
+    const safeSearchLabels = safeSearchResult.safeSearchAnnotation;
+    const isCat = labels.some(
+      (label: LabelType) =>
+        catLabelsArray.indexOf(label.description.toLowerCase()) >= 0
+    );
+    const isNSFW =
+      Object.values(safeSearchLabels).includes(
+        SafeSearchLikelihoods.VERY_LIKELY
+      ) ||
+      Object.values(safeSearchLabels).includes(SafeSearchLikelihoods.LIKELY);
+    console.log({ isCat, isNSFW });
   });
